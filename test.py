81ed3b4ba0f133168f482c41faff312d3f8baeff
pango322 <ipango322@gmail.com>
2026-08-30 15:48:21 +0400

    working clicker

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
-import pyautogui
-import keyboard
+#systemctl --user enable --now ydotool.service
 import time
+import keyboard
+import subprocess
 
 while True:
     if keyboard.is_pressed("x"):
-        pyautogui.click()
+        subprocess.run(["ydotool", "click", "0xC0"])
         time.sleep(0.03)
     else:
         time.sleep(0.01)
